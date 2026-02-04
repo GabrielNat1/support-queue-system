42b789499f34d460813a4bec589391198816fc46
GabrielNat1 <dcbiel846@gmail.com>
2026-02-04 10:15:43 -0300

    feat: first commit

---
+++ src/main/java/com/example/support_queue_system/SupportQueueSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.support_queue_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportQueueSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportQueueSystemApplication.class, args);
+	}
+
+}
