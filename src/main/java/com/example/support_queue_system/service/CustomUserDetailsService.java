ce8c2c0eb9cbb12384e5c6c7c1bd9de4a0a1d4d3
GabrielNat1 <dcbiel846@gmail.com>
2026-02-06 20:11:38 -0300

    feat: create customUserDetailsServer

--- src/main/java/com/example/support_queue_system/service/CustomUserDetailsService.java
+++ src/main/java/com/example/support_queue_system/service/CustomUserDetailsService.java
@@ -1,4 +1,29 @@
 package com.example.support_queue_system.service;
 
-public class CustomUserDetailsService {
+import com.example.support_queue_system.model.User;
+import com.example.support_queue_system.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(() ->
+                new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().replace("ROLE_", ""))
+                .build();
+    };
 }
